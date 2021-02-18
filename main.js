b99e5266a2dfb631f194ef742f2ea811d58be16f
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-18 14:59:06 -0500

    created function 'catStrings'. called in 2 variables tested by Quokka

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ let quest2 = superQuestion("sup, dude");
 
 quest1
 quest2
+
+function catStrings(str1, str2){
+    return str1 + ' ' + str2
+}
+cat1 = catStrings("Hi","dude!")
+cat2 = catStrings("happy happy", "joy joy")
+
+cat1
+cat2
