d89a4c1290bb91bb83dbe817ae51fde3bac9e960
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-18 15:05:39 -0500

    created function 'tripleSub'. Called in 3 variables tested by Quokka

--- main.js
+++ main.js
@@ -16,6 +16,17 @@ cat2 = catStrings("happy happy", "joy joy");
 cat1;
 cat2;
 
+function tripleSub(num1, num2, num3) {
+	return num1 - num2 - num3;
+}
+sub1 = tripleSub(10, 1, 2);
+sub2 = tripleSub(8, 3, 5);
+sub3 = tripleSub(100, 80, 25);
+
+sub1;
+sub2;
+sub3;
+
 function tripleSubRev(num1, num2, num3) {
 	return num3 - num2 - num1;
 }
